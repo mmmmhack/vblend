714702a90e4503e02dbd1a110dbdc951235b9822
William Knight <william.knight@gmail.com>
2012-04-23 13:14:40 -0600

    updating linux makefile scripts

--- util/gen_linux_from_osx_makefile.lua
+++ util/gen_linux_from_osx_makefile.lua
@@ -1,8 +1,10 @@
 -- gen_linux_from_osx_makefile.lua  : generates Makefile.linux from Makefile.osx
 
+require('debugger')
+
 local this_script = "gen_linux_from_osx_makefile.lua"
 
-local subs = {
+local global_subs = {
   ['Mac OSX makefile'] = 'linux makefile',
   ['Makefile%.osx'] = 'Makefile.linux',
   ['%-dynamiclib %-undefined dynamic_lookup'] = '-shared -Wl,-soname,$(PREFIX)/$(notdir $@)',
@@ -10,16 +12,41 @@ local subs = {
   ['%-framework OpenGL'] = '-lGL -lGLU',
   ['SO=so'] = 'SO=so',
   ['EXE='] = 'EXE=',
---  ['%.%./util/sys%.$%(SO%)'] = '',
---  ['libglfw/libglfw%.$%(SO%)'] = '', 
---  ['%.%./img/img.$%(SO%)'] = '',
---  ['%.%./font/tfont%.$%(SO%)'] = '', 
-
---  ['lua_libs='] = 'lua_libs=$(lua_dir)/lua51.dll',
 }
 
 -- math.huge works funky 
 local math_real_big = 10000000000
+
+function read_local_subs(fname)
+	local local_subs = {}
+	local pat = "^# local_sub %['(.+)'%] = '(.+)'$"
+	for ln in io.open(fname, "r"):lines() do
+--		local match = string.match(ln, pat)		-- this returns literal string match instead of captures...?
+		local b, e, k, v = string.find(ln, pat)
+		if b then
+			local_subs[k] = v
+		end
+	end
+	return local_subs
+end
+
+function apply_subs(buf_lines, subs)
+	local sub_lines = {}
+	for i = 1, #buf_lines do
+		local ln = buf_lines[i]
+		local skip_line = false
+		if string.match(ln, "^#") then
+			skip_line = true
+		end
+		if not skip_line then
+			for pat, repl in pairs(subs) do
+				ln = string.gsub(ln, pat, repl)
+			end
+		end
+		sub_lines[i] = ln
+	end
+	return sub_lines
+end
 
 function main()
   if #arg < 1 then
@@ -27,18 +54,25 @@ function main()
     os.exit(1)
   end
   local makefile_in = arg[1]
---print(string.format("reading %s...", makefile_in))
-  io.input(makefile_in) 
-  local in_buf = io.read(math_real_big)
---print(string.format("read %d bytes", #buf))
-  local buf = in_buf
+
+	local local_subs = read_local_subs(makefile_in) 
+
+--  io.input(makefile_in) 
+ -- local in_buf = io.read(math_real_big)
+ 	local in_lines = {}
+	for ln in io.open(makefile_in, "r"):lines() do
+		in_lines[#in_lines + 1] = ln
+	end
   io.close()
-  for pat, repl in pairs(subs) do
-    buf = string.gsub(buf, pat, repl)
-  end
---  print("done")
+
+  local buf_lines = in_lines
+	buf_lines = apply_subs(buf_lines, local_subs)
+	buf_lines = apply_subs(buf_lines, global_subs)
+
 	print(string.format("### auto-generated from %s by %s\n", makefile_in, this_script))
-  print(buf)
+	for i = 1, #buf_lines do
+	  print(buf_lines[i])
+	end
 end
 
 main()
